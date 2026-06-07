cb2377db07d9a059a7e3d8ec60adae458bdd1cff
aminemoussi <amine@19labs.com>
2026-06-08 00:43:45 +0100

    loading the map chnged + no rendering settings

--- carla/record_imu.py
+++ carla/record_imu.py
@@ -22,11 +22,12 @@ def main():
     client = carla.Client("localhost", 2000)
     client.set_timeout(300.0)
 
-    world = client.load_world("Town03")
+    world = client.get_world()
 
     settings = world.get_settings()
     settings.synchronous_mode = True
     settings.fixed_delta_seconds = 0.00825
+    settings.no_rendering_mode = True
     world.apply_settings(settings)
 
     traffic_manager = client.get_trafficmanager(8000)
